0a243866c1033c3fab53205882d287b85a8050d2
Daniil Akhiyan <daniil.axiyan.316@mail.ru>
2023-02-27 23:47:46 +0300

    feat: answear

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
 // You should implement your task here.
 
+
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix == undefined || matrix.length === 0){
+    return [];
+  }
+  let arrayMatr = [];
+  matrix.forEach((el, i) => {
+    if(i % 2 === 0){
+      arrayMatr.push(...el);
+    }else{
+      for (let index = el.length-1; index >= 0; index--) {
+        const element = el[index];
+        arrayMatr.push(element);
+      }
+    }
+  });
+  return arrayMatr
+
 }
